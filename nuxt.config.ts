8ce4139b063e195809868211cb6750cd914f2511
mike <cspkno005@gmail.com>
2024-11-04 04:14:31 +0800

    update setup sass-embedded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-escss-estest']
+  modules: ['nuxt-escss-estest'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+          additionalData: `@use '/assets/styles/_awaken.scss' as *;`,
+        },
+      },
+    },
+  }
 })
