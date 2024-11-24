5d4bb02092f7f27ea32c3a468abfed89f1d803ff
mike <cspkno005@gmail.com>
2024-11-24 15:57:17 +0800

    update lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-escss-estest-true'],
+  modules: ['nuxt-escss-estest-visible'],
   vite: {
     css: {
       preprocessorOptions: {
